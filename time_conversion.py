728fd5f90a65ab330fae39b5237f57d79a917c8c
Matthew Summers <imadethisforgithub@gmail.com>
2023-08-28 15:56:07 -0500

    better error handling

--- time_conversion.py
+++ time_conversion.py
@@ -8,31 +8,31 @@ def timeConversion(s):
         time_regex = r"^(1[0-2]|0?[1-9]):[0-5][0-9]:[0-5][0-9]([ap]m)?$"
         matches = re.findall(time_regex, s, re.IGNORECASE)
 
-    if matches:
-        time = s.split(":")
-        hour = time[0]
-        time_mark = time[2][2:].upper()
+        if matches:
+            time = s.split(":")
+            hour = time[0]
+            time_mark = time[2][2:].upper()
 
-        if time_mark == "PM":
-            if hour != "12":
-                hour = int(hour)
-                hour += 12
-                time[0] = str(hour)
-        elif time_mark == "AM":
-            if hour == "12":
-                time[0] = "00"
+            if time_mark == "PM":
+                if hour != "12":
+                    hour = int(hour)
+                    hour += 12
+                    time[0] = str(hour)
+            elif time_mark == "AM":
+                if hour == "12":
+                    time[0] = "00"
 
-        new_time = ":".join(time)
-        return new_time[:-2]
+            new_time = ":".join(time)
+            return new_time[:-2]
 
     else:
-        return "Time should be formatted 00:00:00AM / 00:00:00PM"
+        return "Time string should be formatted as '00:00:00AM' / '00:00:00PM'"
 
 
 if __name__ == "__main__":
-    n = input('Provide the time as a string in the format of "12:03:24AM"\n')
-    print(timeConversion(n))
-
+    # n = input('Provide the time as a string in the format of "12:03:24AM"\n')
+    # print(timeConversion(n))
+    print(timeConversion(2))
 # Sample cases 
     # input_time_1 = "03:45:30PM"
     # input_time_2 = "12:30:15AM"
